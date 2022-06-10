70b0dd2ba979b4b245e2e2a14c50a170a7fe7f06
Shan Jia <73159723+Js11252001@users.noreply.github.com>
2022-06-10 10:23:04 +0800

    add multiple operator

--- src/com/company/Calculator.java
+++ src/com/company/Calculator.java
@@ -4,28 +4,33 @@ import javax.swing.*;
 import java.awt.*;
 import java.awt.event.*;
 import java.io.*;
+import java.util.PriorityQueue;
+import java.util.Queue;
 import java.util.Scanner;
 
 public class Calculator implements ActionListener{
-    Operand operand =new Operand();
+    OperandUI operandUI =new OperandUI();
     Number num = new Number();
-    History history = new History();
+    HistoryUI history = new HistoryUI();
     JFrame frame;
     JTextField textField;
     JPanel panel;
+    Queue<String> queue;
+    boolean isFirst;
 
 
     Font myFont = new Font("宋体",Font.BOLD,30);
 
-    double num1=0,num2=0,result=0;
+    double num1, num2, result;
     char operator;
 
     Calculator(){
-
+        isFirst = true;
         frame = new JFrame("Calculator");
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setSize(420, 550);
         frame.setLayout(null);
+        queue = new PriorityQueue();
 
         textField = new JTextField();
         textField.setBounds(50, 45, 300, 50);
@@ -36,9 +41,9 @@ public class Calculator implements ActionListener{
 
 
         for(int i =0;i<9;i++) {
-            operand.functionButtons[i].addActionListener(this);
-            operand.functionButtons[i].setFont(myFont);
-            operand.functionButtons[i].setFocusable(false);
+            operandUI.functionButtons[i].addActionListener(this);
+            operandUI.functionButtons[i].setFont(myFont);
+            operandUI.functionButtons[i].setFocusable(false);
         }
 
         for(int i =0;i<10;i++) {
@@ -52,11 +57,11 @@ public class Calculator implements ActionListener{
         panel.setBounds(50, 100, 300, 300);
         panel.setLayout(new GridLayout(4,4,10,10));
 
-        operand.AddToPanel(panel,num);
+        operandUI.AddToPanel(panel,num);
 
         frame.add(panel);
         frame.add(history.toolBar);
-        operand.AddToFrame(frame);
+        operandUI.AddToFrame(frame);
         frame.add(textField);
         frame.setVisible(true);
 
@@ -109,57 +114,116 @@ public class Calculator implements ActionListener{
                 textField.setText(textField.getText().concat(String.valueOf(i)));
             }
         }
-        if(e.getSource()==operand.decButton) {
+        if(e.getSource()== operandUI.decButton) {
             textField.setText(textField.getText().concat("."));
         }
-        if(e.getSource()==operand.addButton) {
-            num1 = Double.parseDouble(textField.getText());
-            operator ='+';
-            textField.setText("");
+        if(e.getSource()== operandUI.addButton) {
+            if (isFirst) {
+                num1 = Double.parseDouble(textField.getText());
+                operator = '+';
+                queue.add(num1 + " + ");
+                textField.setText("");
+                isFirst = false;
+            }
+            else {
+                num2 = Double.parseDouble(textField.getText());
+                num1 = switchOperator(num1, num2, operator);
+                operator = '+';
+                queue.add(num2 + " + ");
+                textField.setText("");
+            }
         }
-        if(e.getSource()==operand.subButton) {
-            num1 = Double.parseDouble(textField.getText());
-            operator ='-';
-            textField.setText("");
+        if(e.getSource()== operandUI.subButton) {
+            if (isFirst) {
+                num1 = Double.parseDouble(textField.getText());
+                operator = '-';
+                queue.add(num1 + " - ");
+                textField.setText("");
+                isFirst = false;
+            }
+            else {
+                num2 = Double.parseDouble(textField.getText());
+                num1 = switchOperator(num1, num2, operator);
+                operator = '-';
+                queue.add(num2 + " - ");
+                textField.setText("");
+            }
         }
-        if(e.getSource()==operand.mulButton) {
-            num1 = Double.parseDouble(textField.getText());
-            operator ='*';
-            textField.setText("");
+        if(e.getSource()== operandUI.mulButton) {
+            if (isFirst) {
+                num1 = Double.parseDouble(textField.getText());
+                operator = '*';
+                queue.add(num1 + " * ");
+                textField.setText("");
+                isFirst = false;
+            }
+            else {
+                num2 = Double.parseDouble(textField.getText());
+                num1 = switchOperator(num1, num2, operator);
+                operator = '*';
+                queue.add(num2 + " * ");
+                textField.setText("");
+            }
         }
-        if(e.getSource()==operand.divButton) {
-            num1 = Double.parseDouble(textField.getText());
-            operator ='/';
-            textField.setText("");
+        if(e.getSource()== operandUI.divButton) {
+            if (isFirst) {
+                num1 = Double.parseDouble(textField.getText());
+                operator = '/';
+                queue.add(num1 + " / ");
+                textField.setText("");
+                isFirst = false;
+            }
+            else {
+                num2 = Double.parseDouble(textField.getText());
+                num1 = switchOperator(num1, num2, operator);
+                operator = '/';
+                queue.add(num2 + " / ");
+                textField.setText("");
+            }
         }
-        if(e.getSource()==operand.equButton) {
+        if(e.getSource()== operandUI.equButton) {
             num2=Double.parseDouble(textField.getText());
-
-            Operator op = new Operator(num1,num2,result,operator, history.area);
-            num1= op.num1;
-            num2= op.num2;
-            result= op.result;
-
-
-            textField.setText(String.valueOf(result));
-            history.area.append(" = " + result + "\n");
-            num1=result;
+            Operator op = new Operator(num1, num2, result, operator, history.area, queue);
+            textField.setText(String.valueOf(op.result));
+            history.area.append(" = " + op.result + "\n");
+            num1 = 0;
+            num2 = 0;
+            result = 0;
+            isFirst = true;
         }
-        if(e.getSource()==operand.clrButton) {
+        if(e.getSource()== operandUI.clrButton) {
             textField.setText("");
+            num1 = 0;
+            num2 = 0;
+            result = 0;
+            isFirst = true;
         }
-        if(e.getSource()==operand.delButton) {
+        if(e.getSource()== operandUI.delButton) {
             String string = textField.getText();
             textField.setText("");
             for(int i=0;i<string.length()-1;i++) {
                 textField.setText(textField.getText()+string.charAt(i));
             }
         }
-        if(e.getSource()==operand.negButton) {
+        if(e.getSource()== operandUI.negButton) {
             double temp = Double.parseDouble(textField.getText());
             temp*=-1;
             textField.setText(String.valueOf(temp));
         }
         history.CheckAction(e);
+    }
+
+    public double switchOperator(double num1, double num2, char operator) {
+        switch (operator) {
+            case '+':
+                return num1 + num2;
+            case '-':
+                return num1 - num2;
+            case '*':
+                return num1 * num2;
+            case '/':
+                return num1 / num2;
+        }
+        return 0;
     }
 }
--- src/com/company/Operator.java
+++ src/com/company/Operator.java
@@ -1,36 +1,42 @@
 package com.company;
 
 import javax.swing.*;
+import java.util.Queue;
 
 public class Operator {
     double num1=0,num2=0,result=0;
     char operator;
 
-    Operator(double num1, double num2, double result, char operator, JTextArea area){
+    Operator(double num1, double num2, double result, char operator, JTextArea area, Queue<String> queue){
         this.num1 = num1;
         this.num2 = num2;
         this.result = result;
         this.operator = operator;
-        f(area);
+        f(area, queue);
     }
 
-    public void f(JTextArea area){
+    public void f(JTextArea area, Queue<String> queue){
+
+        while (queue.size() > 0) {
+            String element = queue.poll();
+            area.append(element);
+        }
         switch(operator) {
             case'+':
                 result=num1+num2;
-                area.append( num1 + " + " + num2);
+                area.append("" + num2);
                 break;
             case'-':
                 result=num1-num2;
-                area.append(num1 + " - " + num2);
+                area.append("" + num2);
                 break;
             case'*':
                 result=num1*num2;
-                area.append(num1 + " * " + num2);
+                area.append("" + num2);
                 break;
             case'/':
                 result=num1/num2;
-                area.append(num1 + " / " + num2);
+                area.append("" + num2);
                 break;}
     }
 }
