83974444bfefef22b707e87d15119ddc70980e6b
Shan Jia <73159723+Js11252001@users.noreply.github.com>
2022-06-15 16:28:49 +0800

    Delete src/com/company directory

--- src/com/company/Driver.java
+++
@@ -1,7 +0,0 @@
-package com.company;
-
-public class Driver {
-    public static void main(String[] args) {
-        Calculator calc = new Calculator();
-    }
-}
--- src/com/company/Number.java
+++
@@ -1,8 +0,0 @@
-package com.company;
-
-import javax.swing.*;
-
-public class Number {
-    JButton[] numberButtons = new JButton[10];
-
-}
